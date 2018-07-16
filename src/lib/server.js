01e604823ac6a70a9b16e6fcbee6a8b148f2c0ab
Noah Israel <noahmvf@gmail.com>
2018-07-15 23:39:51 -0700

    tests passing

--- src/lib/server.js
+++ src/lib/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 let server = null;
 
 
-// third party apps
+// third party app
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
